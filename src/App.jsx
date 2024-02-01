779e5802dd3cfab3f48c63b9cf686ebcc43a3feb
Marcin Kacperski <kacperski81@gmail.com>
2024-02-01 18:37:45 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import imgSrc from './abc.png'
+
+function App() {
+  return (
+    <div>
+      <h2>Test images</h2>
+      <img src={imgSrc} style={{width:"200px"}}/>
+    </div>
+  )
+}
+
+export default App
